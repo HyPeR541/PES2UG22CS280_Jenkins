d25ba195f8cec270eb3de40936e57f5fa93a6d70
Lohit <klohit72@gmail.com>
2025-03-10 16:35:13 +0530

    made changes in the cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout <<"yes i made the change ";
   return 0;
 }
